539d83158152db4f6c01bc67a34abbb40b1bc7f9
ARGUS269 <akoubaki.314@gmail.com>
2026-09-22 12:44:28 +0000

    feat: initial project setup

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,43 +1,7 @@
 <script setup>
 import { ref } from "vue";
 
-const emit = defineEmits(["SubmitClicked"]);
-
-function cargoSubmit() {
-  console.log(destinationCity.value);
-
-  if (destinationCity.value === "") {
-    inputError.value = true;
-    return;
-  }
-  launch.value = "---------------------";
-  isLaunched.value = true;
-  const intervalID = setInterval(() => {
-    emit("submit-clicked", {
-      id: `TRK-${isDuplicated(Date.now().toString().slice(-4))}`,
-      destination: destinationCity.value,
-      cargoType: categorization.value,
-      weight: massValue.value,
-      value: valValue.value,
-      status: "MANIFEST_CREATED",
-      transitProgress: 0,
-    });
-    categorization.value = "Electronics";
-    destinationCity.value = "";
-    massValue.value = 10;
-    valValue.value = 500;
-    clearInterval(intervalID);
-  }, 2000);
-}
-
-function testInput() {
-  inputError.value = false;
-
-  if (destinationCity.value === "") {
-    inputError.value = true;
-    return;
-  }
-}
+const isShow = ref(false);
 </script>
 
 <template>
@@ -55,15 +19,23 @@ function testInput() {
       @input="testEmailInput"
     />
     <label for="password">Password: </label>
-    <input
-      type="password"
-      name="password"
-      id="password"
-      class="email"
-      placeholder="........"
-      v-model="passwordField"
-      @input="testPasswordInput"
-    />
+    <div class="pass">
+      <input
+        name="password"
+        id="password"
+        class="email password"
+        placeholder="........"
+        v-model="passwordField"
+        @input="testPasswordInput"
+        :type="!isShow ? 'password' : 'text'"
+      />
+      <i
+        class="fa-regular fa-eye-slash"
+        @click="isShow = !isShow"
+        :class="{ 'fa-eye': isShow }"
+      ></i>
+    </div>
+
     <div class="mass-slider-card">
       <label for="scale-slider" class="scale-slider-label" :data-text="massValue"
         >Cargo Mass Scale:</label
@@ -75,7 +47,7 @@ function testInput() {
       :disabled="isLaunched"
       :class="{ launchClass: isLaunched }"
     >
-      {{ launch }}
+      Sign In
     </button>
   </form>
 </template>
@@ -93,14 +65,15 @@ button {
   border: 0;
   border-radius: 6px;
   background-color: #e5e7eb;
+  font-weight: bold;
 }
 
-.errorHundle {
+.email {
   position: relative;
 }
 
-.errorHundle::before {
-  content: "Please enter A destination City";
+.email::before {
+  content: "Please enter A valid Email";
   position: absolute;
   top: -85%;
   left: 10px;
@@ -110,45 +83,26 @@ button {
   color: red;
   margin-top: 5px;
 }
+
+.pass {
+  width: 100%;
+  position: relative;
+}
+
+.pass i {
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  cursor: pointer;
+}
+
+.pass .password {
+  width: 90%;
+}
+
 button:hover {
   background-color: #cccccc;
-}
-
-.launchClass:hover {
-  background-color: #e5e7eb;
-}
-
-.launchClass {
-  font-weight: 700;
-  font-size: 1rem;
-  letter-spacing: 0.4em;
-  position: relative;
-  overflow: hidden;
-  background-color: #e5e7eb;
-}
-
-.launchClass::before {
-  content: "";
-  position: absolute;
-  left: 20px;
-  width: 20px;
-  height: 20px;
-  background-image: url(../assets/truck-regular-full.svg);
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: contain;
-  top: 27%;
-  transform: translateY(-50%);
-  animation: slideIn 2s ease-in normal 1 forwards;
-}
-
-@keyframes slideIn {
-  50% {
-    left: calc(100% - 50px);
-  }
-  100% {
-    left: calc(100% - 50px);
-  }
 }
 
 label {
@@ -161,51 +115,10 @@ label {
   letter-spacing: 0.05em;
 }
 
-.slider-wrapper {
-  position: relative;
-  margin-bottom: 10px;
-}
-
-input[type="range"] {
-  width: 100%;
-  margin: 0;
-  -webkit-appearance: none;
-  appearance: none;
-  height: 8px;
-  border-radius: 9999px;
-  background: #e5e7eb;
-  outline: none;
-}
-
-.scale-ticks span::before {
-  content: "";
-  position: absolute;
-  top: -10px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: 2px;
-  height: 6px;
-  background-color: #d1d5db;
-}
-
 h2,
 p {
   text-align: center;
   margin: 0;
-}
-
-.scale-ticks {
-  display: flex;
-  justify-content: space-between;
-  padding: 0 2px;
-  margin-top: 10px;
-}
-
-.scale-ticks span {
-  font-size: 12px;
-  color: #9ca3af;
-  position: relative;
-  text-align: center;
 }
 
 .email {
@@ -225,14 +138,8 @@ p {
   font-weight: 400;
 }
 
-.select-options li {
-  color: #333333;
-
-  padding: 10px 14px;
-}
-
-.select-options li:hover {
-  background-color: #cccccc;
+.password::placeholder {
+  font-size: 30px;
 }
 
 .form-container {
@@ -244,60 +151,5 @@ p {
   box-shadow: 0 4px 15px rgba(0, 0, 0, 0.6);
   padding: 30px;
   background-color: white;
-}
-
-.custom-select {
-  position: relative;
-}
-
-.select-options {
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-  list-style: none;
-  cursor: pointer;
-  position: absolute;
-  width: 100%;
-  margin: 0;
-  padding: 0;
-  background-color: white;
-  border-radius: 6px;
-  border: 1px solid #cccccc;
-  margin-top: 5px;
-  transform-origin: center top;
-  z-index: 10;
-}
-
-.shrink-square-enter-active,
-.shrink-square-leave-active {
-  transition:
-    opacity 0.3s ease,
-    transform 0.3s ease;
-}
-
-.shrink-square-enter-from,
-.shrink-square-leave-to {
-  opacity: 0;
-  transform: scale(0.4);
-}
-
-.select-trigger {
-  display: flex;
-  justify-content: space-between;
-  cursor: pointer;
-
-  color: #333333;
-
-  padding: 10px 14px;
-  border: 1px solid #cccccc;
-  border-radius: 6px;
-}
-
-.select-trigger div i {
-  transition: transform 0.3s ease;
-}
-
-.rotated-state {
-  transform: rotate(180deg);
 }
 </style>
